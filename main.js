a60ec859f08ad0161fabcbf72dff8bf2eb008682
Bojack8789 <106709451+Bojack8789@users.noreply.github.com>
2023-12-01 16:39:31 -0300

    Se cambió la lógica del programa en main2.js y se modificaron las funciones para que no interfieran entre sí, haciendo la función animar scroll recursiva.

--- main.js
+++ main.js
@@ -11,128 +11,245 @@
       console.log(grupo);
     }
   }*/
-  var myElement = document.getElementById('boton');
-  console.log(myElement);
+
+let ultimaPosicion = 0;
 
 
 function mostrarMain() {
     // Hacer visible el Main 2
     document.getElementById("main1").style.display = "none";
     document.getElementById("main2").style.display = "block";
-    console.log("esta funcion funciona ")
+    console.log("esta funcion funciona ");
     agregarPalabras();
     inicializarScroll();
     aplicarEscala();
+}
 
-  }
-
-  function cerrarMain2() {
+function cerrarMain2() {
     // Ocultar el Main 2 y restablecer el input de texto
     document.getElementById("main2").style.display = "none";
-    document.getElementById("main1").style.display = "block";}
+    document.getElementById("main1").style.display = "block";
+}
 
+// Lógica del renderizado
+function agregarPalabras() {
+    let texto = document.getElementById('texto').value;
+    let palabras = texto.split(' ');
 
+    let containerPalabras = document.getElementById('container-palabras');
 
-    // a partir de aquei va la logica del renderizado 
+    containerPalabras.innerHTML = ''; // Limpia el contenido anterior
 
-    function agregarPalabras() {
-      let texto = document.getElementById('texto').value;
-      let palabras = texto.split(' ');
+    palabras.forEach(function(palabra) {
+        let palabraElemento = document.createElement('div');
+        palabraElemento.classList.add('palabra');
+        palabraElemento.textContent = palabra;
+
+        containerPalabras.appendChild(palabraElemento);
+    });
+}
+
+function inicializarScroll() {
+    let contenedor = document.getElementById('container-palabras');
+    // aca modifico lo de la barra de progreso 
+    let progressBar = document.getElementById('progressBar');
+    progressBar.addEventListener('input', function() {
+        // Calcula la nueva posición basada en el valor de la barra de progreso
+        let nuevaPosicion = (progressBar.value / 100) * contenedor.scrollHeight;
+
+        // Actualiza la última posición
+        ultimaPosicion = nuevaPosicion;
+
+        // Actualiza la posición del contenedor
+        contenedor.scrollTop = ultimaPosicion;
+        
+    });
+
     
-      let containerPalabras = document.getElementById('container-palabras');
-      
-      containerPalabras.innerHTML = ''; // Limpia el contenido anterior
-    
-      palabras.forEach(function(palabra) {
-          let palabraElemento = document.createElement('div');
-          palabraElemento.classList.add('palabra');
-          palabraElemento.textContent = palabra;
-         
-          containerPalabras.appendChild(palabraElemento);
-      });
+
+    // Agrega el evento 'change' al elemento progressBar
+    progressBar.addEventListener('change', function() {
+        // Puedes hacer algo adicional cuando el usuario suelta la barra de progreso
+        console.log('Valor de la barra de progreso:', progressBar.value);
+    });
+    // hasta aca 
+
+    // modificando input velocidad palabras 
+let inputRange = document.getElementById('palabrasPorMinuto');
+let valorDinamico = parseFloat(inputRange.value);
+let animacionActiva = true;
+let velocidadAnimacion = valorDinamico;
+let ultimaPosicion = 0;
+
+inputRange.addEventListener('input', function() {
+    valorDinamico = parseFloat(inputRange.value);
+    velocidadAnimacion = valorDinamico;
+
+    // Llama a la función animarScroll con la nueva velocidad
+    animarScroll(velocidadAnimacion);
+});
+
+inputRange.addEventListener('change', function() {
+    // Puedes hacer algo con el valor, por ejemplo, imprimirlo en la consola
+});
+
+// Función para actualizar la velocidad de desplazamiento
+function updateScrollSpeed() {
+    velocidadAnimacion = inputRange.value;
+  }
+
+  // Evento para detectar cambios en el control de velocidad
+  inputRange.addEventListener('input', updateScrollSpeed);
+
+let mitadContenedor = contenedor.clientHeight / 2;
+
+function animarScroll(velocidad) {
+    if (animacionActiva) {
+        ultimaPosicion += velocidad;
+        contenedor.scrollTop = ultimaPosicion;
+
+        let progreso = (ultimaPosicion / contenedor.scrollHeight) * 100;
+        progressBar.value = progreso;
+        document.getElementById('progressText').textContent = `${Math.round(progreso)}%`;
+
+        requestAnimationFrame(function() {
+            animarScroll(velocidad);
+        });
     }
-    
-    function inicializarScroll() {
-      let contenedor = document.getElementById('container-palabras');
-      let animacionActiva = true;
-      let ultimaPosicion = 0;
-      let velocidadAnimacion = 3; // Puedes ajustar la velocidad según tus necesidades
-      
-      //modificado
-      let mitadContenedor = contenedor.clientHeight / 2;
-      
-      //modificado
-      function animarScroll() {
-          if (animacionActiva) {
-              ultimaPosicion += velocidadAnimacion; // Modificar la velocidad aquí
-              contenedor.scrollTop = ultimaPosicion;
-              requestAnimationFrame(animarScroll);
-          }
-      }
-    
-      function detenerAnimacion() {
-          animacionActiva = false;
-      }
-    
-      function reanudarAnimacion() {
-          animacionActiva = true;
-          animarScroll();
-      }
-    
-      contenedor.addEventListener('touchstart', function() {
-          detenerAnimacion();
-          ultimaPosicion = contenedor.scrollTop;
-      });
-    
-      contenedor.addEventListener('touchend', function() {
-          reanudarAnimacion();
-      });
-    
-      contenedor.addEventListener('scroll', function() {
-          ultimaPosicion = contenedor.scrollTop;
-      });
-    
-      animarScroll();
-    
-      function imprimirUltimaPosicion() {
-          setInterval(function() {
+}
+
+// Inicia la animación con la velocidad inicial
+animarScroll(velocidadAnimacion);
+
+
+
+
+    function detenerAnimacion() {
+        animacionActiva = false;
+    }
+
+    function reanudarAnimacion() {
+        animacionActiva = true;
+        animarScroll(valorDinamico);
+    }
+
+    contenedor.addEventListener('touchstart', function() {
+        detenerAnimacion();
+        ultimaPosicion = contenedor.scrollTop;
+    });
+
+    contenedor.addEventListener('touchend', function() {
+        reanudarAnimacion();
+    });
+
+    contenedor.addEventListener('scroll', function() {
+        ultimaPosicion = contenedor.scrollTop;
+    });
+
+    animarScroll(valorDinamico);
+
+    function imprimirUltimaPosicion() {
+        setInterval(function() {
             console.log(ultimaPosicion);
             console.log("mitad posicion" + mitadContenedor);
-          }, 500);
-      }
-      imprimirUltimaPosicion();
+            console.log('Valor dinámico:', valorDinamico);
+        }, 500);
     }
-    
-    function aplicarEscala() {
-      let contenedor = document.getElementById("container-palabras");
-      let palabras = document.querySelectorAll(".palabra");
-    
-      contenedor.addEventListener("scroll", function() {
+    imprimirUltimaPosicion();
+
+   // botones de rrepro desde aqui 
+
+// Variable para controlar el estado de la animación
+let animacionPausada = false;
+
+function retroceder() {
+  console.log("Retroceder llamado");
+  ultimaPosicion -= 0.1 * contenedor.clientHeight;
+  contenedor.scrollTop = ultimaPosicion;
+
+  // Actualiza la posición de la barra de desplazamiento
+  let progreso = (ultimaPosicion / contenedor.scrollHeight) * 100;
+  progressBar.value = progreso;
+
+  // Si la animación está pausada, reanuda la animación
+  if (animacionPausada) {
+    animacionPausada = false;
+    animarScroll(valorDinamico);
+  }
+}
+  let pausastate = false;
+
+function pausar() {
+  // Cambia el estado a true o false al hacer clic en el botón
+  pausastate = !pausastate;
+  console.log(pausastate)
+  // Llama a la función correspondiente según el estado
+  if (pausastate) {
+    detenerAnimacion();
+  } else {
+    reanudarAnimacion();
+  }
+}
+
+
+function avanzar() {
+  console.log("Avanzar llamado");
+  ultimaPosicion += 0.1 * contenedor.clientHeight;
+  contenedor.scrollTop = ultimaPosicion;
+
+  // Actualiza la posición de la barra de desplazamiento
+  let progreso = (ultimaPosicion / contenedor.scrollHeight) * 100;
+  progressBar.value = progreso;
+
+  // Si la animación está pausada, reanuda la animación
+  if (animacionPausada) {
+    animacionPausada = false;
+    animarScroll(valorDinamico);
+  }
+}
+
+document.getElementById('retrocederBtn').addEventListener('click', retroceder);
+document.getElementById('pausarBtn').addEventListener('click', pausar);
+document.getElementById('avanzarBtn').addEventListener('click', avanzar);
+
+
+}
+
+function aplicarEscala() {
+    let contenedor = document.getElementById("container-palabras");
+    let palabras = document.querySelectorAll(".palabra");
+
+    contenedor.addEventListener("scroll", function() {
         let mitadContenedor = contenedor.clientHeight / 2;
         let rangoInicio = mitadContenedor - (mitadContenedor * 0.2); // 40% del contenedor
         let rangoFin = mitadContenedor + (mitadContenedor * 0.2); // 60% del contenedor
-    
+
         palabras.forEach(function(palabra) {
-          // Calcular la posición de cada palabra
-          let palabraOffsetTop = palabra.offsetTop + palabra.clientHeight / 2 - contenedor.scrollTop - mitadContenedor;
-    
-          // Aplicar escala solo cuando está en el rango del 40% al 60%
-          if (palabraOffsetTop >= rangoInicio && palabraOffsetTop <= rangoFin) {
-            // Calcular la distancia entre la posición de la palabra y el 50%
-            let distancia = Math.abs(mitadContenedor - palabraOffsetTop);
-            
-            // Ajustar el tamaño de la palabra en relación con la distancia
-            let escala = 1 + (1 - distancia / mitadContenedor);
-            palabra.style.transform = "scale(" + escala + ")";
-          } else {
-            palabra.style.transform = "scale(1)";
-          }
+            // Calcular la posición de cada palabra
+            let palabraOffsetTop = palabra.offsetTop + palabra.clientHeight / 2 - contenedor.scrollTop - mitadContenedor;
+
+            // Aplicar escala solo cuando está en el rango del 40% al 60%
+            if (palabraOffsetTop >= rangoInicio && palabraOffsetTop <= rangoFin) {
+                // Calcular la distancia entre la posición de la palabra y el 50%
+                let distancia = Math.abs(mitadContenedor - palabraOffsetTop);
+
+                // Ajustar el tamaño de la palabra en relación con la distancia
+                let escala = 1 + (1 - distancia / mitadContenedor);
+                palabra.style.transform = "scale(" + escala + ")";
+            } else {
+                palabra.style.transform = "scale(1)";
+            }
         });
-      });
-    }
+    });
+
     
-    // Agregar eventos onclick
-    document.getElementById('boton').addEventListener('click', agregarPalabras);
-    document.getElementById('boton').addEventListener('click', inicializarScroll);
-    document.getElementById('boton').addEventListener('click', aplicarEscala);
-    
+}
+
+
+
+// Agregar eventos onclick
+document.getElementById('boton').addEventListener('click', agregarPalabras);
+document.getElementById('boton').addEventListener('click', inicializarScroll);
+document.getElementById('boton').addEventListener('click', aplicarEscala);
+
+
---
+++ main2.js
@@ -0,0 +1,148 @@
+function mostrarMain() {
+    // Hacer visible el Main 2
+    document.getElementById("main1").style.display = "none";
+    document.getElementById("main2").style.display = "block";
+    console.log("esta funcion funciona ");
+    agregarPalabras();
+    scrollAutomatico();
+}
+
+function cerrarMain2() {
+    // Ocultar el Main 2 y restablecer el input de texto
+    document.getElementById("main2").style.display = "none";
+    document.getElementById("main1").style.display = "block";
+}
+
+// Lógica del renderizado
+function agregarPalabras() {
+    let texto = document.getElementById('texto').value;
+    let palabras = texto.split(' ');
+
+    let containerPalabras = document.getElementById('container-palabras');
+
+    containerPalabras.innerHTML = ''; // Limpia el contenido anterior
+
+    palabras.forEach(function(palabra) {
+        let palabraElemento = document.createElement('div');
+        palabraElemento.classList.add('palabra');
+        palabraElemento.textContent = palabra;
+
+        containerPalabras.appendChild(palabraElemento);
+    });
+}
+
+
+//variables 
+
+const containerPalabras = document.getElementById('container-palabras');
+
+
+
+
+const progressBar = document.getElementById('progressBar');
+
+const avanzarBtn = document.getElementById('avanzarBtn');
+//btn pausa en la funcion de pausa PENDIENTE TRAER AQUI 
+const retrocederBtn = document.getElementById('retrocederBtn');
+
+
+
+
+
+
+let pausa = false; // Variable para controlar el estado de pausa
+
+
+let inputVelocidad = document.getElementById('palabrasPorMinuto');
+        velocidadPalabras = inputVelocidad.value;
+
+
+//CONTROLADOR TOUCH Y SCROOLL FLUIDES 
+
+  
+    // Evento de touchstart para dispositivos táctiles
+    containerPalabras.addEventListener('touchstart', function() {
+        pausa = true;
+      });
+    
+      // Evento de touchend para dispositivos táctiles
+      containerPalabras.addEventListener('touchend', function() {
+        pausa = false;
+      });
+
+      containerPalabras.addEventListener('wheel', function() {
+        pausa = true;
+        setTimeout(function() {
+            pausa = false;
+            console.log('¡Pausa cambiada a false!');
+          }, 200); // 200 milisegundos es igual a 0.2 segundos
+      });
+
+
+
+     // scrollo auto 
+
+     let velocidad = 1; // Valor inicial de velocidad
+
+    function scrollAutomatico() {
+      const containerPalabras = document.getElementById('container-palabras');
+      const speed = 1000 / velocidad; // Calcula la velocidad en milisegundos
+
+      if (!pausa) {
+        containerPalabras.style.scrollBehavior = `scroll ${speed}ms linear`;
+        containerPalabras.scrollTop += 1; // Scroll hacia abajo
+      }
+
+      // Llama recursivamente a la función para seguir el scroll
+      setTimeout(scrollAutomatico, speed);
+    }
+
+    function cambiarVelocidad() {
+      const inputVelocidad = document.getElementById('palabrasPorMinuto');
+      velocidad = inputVelocidad.value;
+    }
+
+    
+
+
+ // Actualizar el valor del input al desplazar el scroll del div
+ containerPalabras.addEventListener('scroll', function () {
+    const scrollPercentage = (containerPalabras.scrollTop / (containerPalabras.scrollHeight - containerPalabras.clientHeight)) * 100;
+    progressBar.value = scrollPercentage;
+  });
+
+  // Actualizar el desplazamiento del div al cambiar el valor del input
+  progressBar.addEventListener('input', function () {
+    const scrollValue = (this.value / 100) * (containerPalabras.scrollHeight - containerPalabras.clientHeight);
+    containerPalabras.scrollTop = scrollValue;
+  });
+
+  // Desplazar hacia adelante un 5% del scrollHeight al hacer clic en el botón de avanzar
+  avanzarBtn.addEventListener('click', function () {
+    const nuevoValor = containerPalabras.scrollTop + (0.05 * containerPalabras.scrollHeight);
+    containerPalabras.scrollTop = nuevoValor;
+    console.log("Nuevo scrollTop:", nuevoValor); });
+     
+    // Desplazar hacia atrás un 5% del scrollHeight al hacer clic en el botón de retroceder
+  retrocederBtn.addEventListener('click', function () {
+    const nuevoValor = containerPalabras.scrollTop - (0.05 * containerPalabras.scrollHeight);
+    containerPalabras.scrollTop = nuevoValor;
+    console.log("Nuevo scrollTop:", nuevoValor);
+  });
+
+
+// Controlador de eventos para el botón de pausa/reanudar
+document.getElementById("pausaPlaybtn").addEventListener("click", function () {
+    pausa = !pausa; // Cambia el estado de pausa al hacer clic en el botón
+    console.log(pausa);
+    // Cambia el ícono del botón según el estado de pausa
+    
+});
+
+
+
+//HATA AQUI TODO PERFECTO ! 
+
+
+//SCALA 
+
